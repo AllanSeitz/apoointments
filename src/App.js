24a0781aa09fe73ca2f953e1d83854a748bd7817
Allan <allan@Allans-MBP.localdomain>
2018-12-12 16:30:06 -0500

    todo list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Todo from "./Todo.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Todo />
         <header>Add to the list</header>
       </div>
     );
