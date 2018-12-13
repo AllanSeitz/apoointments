e44fdb78f856e7beabdb9900e9a3e420e2c0e22e
Allan <allan@Allans-MacBook-Pro.local>
2018-12-13 13:15:03 -0500

    can add days

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Link = ({ active, children, onClick }) => {
-  if (activce) {
+  if (active) {
     return <span>{children}</span>;
   }
   return (
